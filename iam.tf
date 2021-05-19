97f2472a2f2e730b4435f11097eed8b2c9613b51
cawoodruff <52130002+cawoodruff@users.noreply.github.com>
2021-05-19 14:20:04 -0400

    Update iam.tf
    updated policy name.  removed section of code

--- iam.tf
+++ iam.tf
@@ -1,32 +1,23 @@
 resource "aws_iam_user" "newemployees" {
-count = length(var.iam_names)
-name          = element(var.iam_names,count.index)
+  count = length(var.iam_names)
+  name  = element(var.iam_names,count.index)
   path = "/system/"
+  create_access_keys = true
 
   tags = {
-    tag-key = "tag-value"
+    tag-key = "new hire"
   }
 }
 
-resource "aws_iam_access_key" "newemp" {
+resource "aws_iam_access_key" "newhirekeys" {
   count = length(var.iam_names)
   user  = element(var.iam_names, count.index)
 
   depends_on = [
-    aws_iam_user_policy.newemp_policy
+    aws_iam_user_policy.newhire_policy
   ]
 
 }
-
-
-/*
-resource "aws_iam_user_login_profile" "example" {
-  count = length(var.iam_names)
-  user   = element(var.iam_names,count.index)
-  pgp_key = "keybase:some_person_that-exists" #receiving an error attached to this attribute (Error: error retrieving GPG Key during IAM User Login Profile)
-  password_reset_required = true
-}
-*/
 
 resource "aws_iam_account_password_policy" "strict" {
   minimum_password_length        = 8
@@ -38,7 +29,7 @@ resource "aws_iam_account_password_policy" "strict" {
 }
 
 
-resource "aws_iam_user_policy" "newemp_policy" {
+resource "aws_iam_user_policy" "newhire_policy" {
   count = length(var.iam_names)
   name = "new"
   user = element(var.iam_names,count.index)
