56a8035bd7fde7b703fea760d7d95b5883e253e2
cawoodruff <52130002+cawoodruff@users.noreply.github.com>
2021-05-22 14:24:22 -0400

    Update variables.tf
    troubleshooting

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 #Five new employees user names
 variable "iam_names" {
   type = list
-  default = ["absmith", "bcjones","cdapple","kjohnson","tscott"]
+  #default = ["absmith", "bcjones","cdapple","kjohnson","tscott"]
+  default = ["cwoodruff", "jvb", "marshall", "orok", "jwb"]
 }
